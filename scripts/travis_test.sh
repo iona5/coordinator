ba06c1576bd63733dd1ff30259aff6ffb09b4d10
iona5 <github@ag99.de>
2019-06-02 09:21:37 +0200

    moving test.run_all() to run tests into test/__init__

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -6,7 +6,7 @@ if [ -z $1 ] ; then QGIS_RELEASE=$1 ; fi
 docker run -d --name qgis-${QGIS_RELEASE} -e DISPLAY=:99 qgis/qgis:release-${QGIS_RELEASE}
 ./docker_test_setup.sh qgis-${QGIS_RELEASE}
 sleep 2
-docker exec -t qgis-${QGIS_RELEASE} sh -c "qgis_testrunner.sh coordinator.test.runtests_docker"
+docker exec -t qgis-${QGIS_RELEASE} sh -c "qgis_testrunner.sh coordinator.test.run_all"
 docker stop qgis-${QGIS_RELEASE}
 docker rm qgis-${QGIS_RELEASE}
 
--- test/__init__.py
+++ test/__init__.py
@@ -1,2 +1,14 @@
 # import qgis libs so that ve set the correct sip api version
-import qgis   # pylint: disable=W0611  # NOQA
+import os, sys, qgis   # pylint: disable=W0611  # NOQA
+from qgis.testing import unittest
+
+def run_all(): 
+    loader = unittest.TestLoader()
+    
+    start_dir = os.path.dirname(__file__)
+    suite = loader.discover(start_dir)
+      
+    runner = unittest.TextTestRunner(verbosity = 3, stream=sys.stdout)
+    runner.run(suite)
+    
+
