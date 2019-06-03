ec2858439822a6fafa4114ea5bff58eace72f00e
iona5 <github@ag99.de>
2019-06-03 13:36:31 +0200

    test/build: display the QGIS version in the tests

--- scripts/docker_test_setup.sh
+++ scripts/docker_test_setup.sh
@@ -5,6 +5,9 @@ set -x
 
 CONTAINER=$1
 
+# display version:
+docker exec ${CONTAINER} sh -c "qgis --help 2>&1 | head -n 1"
+
 docker exec ${CONTAINER} sh -c "mkdir -p /tests_directory"
 docker cp coordinator ${CONTAINER}:/tests_directory/coordinator
 docker exec ${CONTAINER} sh -c "qgis_setup.sh coordinator"
