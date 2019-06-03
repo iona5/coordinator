d1feb4719f84e7113a384a40f5925f9734aaeb92
iona5 <github@ag99.de>
2019-06-03 10:26:29 +0200

    help: remove some chapters and add links

--- scripts/build_help.sh
+++ scripts/build_help.sh
@@ -4,6 +4,18 @@ set -ex
 
 if [ ! -z $1 ]; then cd $1; fi
 
-python3 -m markdown2 -x smarty-pants README.md > help/index.html
+python3 -m markdown2 -x smarty-pants,header-ids README.md > help/index.html
 # images src should now be "images/image.png" instead of "help/images/image.png":
 sed -i 's|src=\"help\/images|src=\"images|' help/index.html
+
+# remove some chapters:
+sed -i '/id="coordinator-plugin"/,/id="basics"/d' help/index.html
+
+# replace last chapter with links:
+sed -i '/known-issues-limitations-planned-features/,$d' help/index.html
+echo '<h2 id="help-links">Links</h2>
+<ul>
+<li><a href="https://plugins.qgis.org/plugins/coordinator/">Plugin homepage on plugins.qgis.org</a></li>
+<li><a href="https://github.com/iona5/coordinator">Github page</a></li>
+<li><a href="https://github.com/iona5/coordinator/issues">Bug Reports</a></li>
+</ul>' >> help/index.html
