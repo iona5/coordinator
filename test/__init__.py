42885c7c236759ed502cde40acf502e4298a6a6f
iona5 <github@ag99.de>
2019-07-25 18:21:36 +0200

    tests: added helper function to load layers to CoordinatorTestCase

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,7 @@
 # import qgis libs so that ve set the correct sip api version
 import os, sys, qgis   # pylint: disable=W0611  # NOQA
 from qgis.testing import unittest
+from qgis.core import QgsProject, QgsVectorLayer
 
 def run_all(): 
     loader = unittest.TestLoader()
@@ -15,4 +16,15 @@ class CoordinatorTestCase(unittest.TestCase):
   @classmethod
   def setUpClass(cls):
     super(CoordinatorTestCase, cls).setUpClass()
+    cls.project = QgsProject.instance()
 
+  def addMapLayer(self, mapLayer):
+    self.project.addMapLayer(mapLayer, True)
+    
+  def addVectorLayerFile(self, vectorLayerFile, vectorLayerName):
+    vectorLayer = QgsVectorLayer(vectorLayerFile, vectorLayerName, "ogr")
+    self.addMapLayer(vectorLayer)
+    return vectorLayer
+
+  def addEuropeLayer(self):
+    return self.addVectorLayerFile(os.path.join(os.path.dirname(__file__), "data/europe.geojson" ), "europe")
