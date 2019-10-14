4a3a7d983c34c19af22e96a8875422bf44b7370b
speillet <sebastien.peillet@oslandia.com>
2019-10-14 14:30:37 +0200

    ENH: add french translation

---
+++ i18n/coordinator_fr.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>CT</name>
+    <message>
+        <location filename="../coordinator.py" line="115"/>
+        <source>Open Coordinator</source>
+        <translation>Ouvrir Coordinator</translation>
+    </message>
+    <message>
+        <location filename="../coordinator.py" line="215"/>
+        <source>outside of map extent</source>
+        <translation>en dehors de l&apos;étendue de la carte</translation>
+    </message>
+    <message>
+        <location filename="../coordinator.py" line="317"/>
+        <source>coordinate added</source>
+        <translation>coordonnée ajoutée</translation>
+    </message>
+    <message>
+        <location filename="../coordinator.py" line="319"/>
+        <source>adding coordinate failed</source>
+        <translation>l&apos;ajour d&apos;une coordonnée a échoué</translation>
+    </message>
+</context>
+<context>
+    <name>CoordinatorDockWidget</name>
+    <message>
+        <location filename="../coordinator_dockwidget.py" line="439"/>
+        <source>S</source>
+        <comment>lblSouth</comment>
+        <translation>S</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget.py" line="439"/>
+        <source>N</source>
+        <comment>lblNorth</comment>
+        <translation>N</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget.py" line="447"/>
+        <source>W</source>
+        <comment>lblWest</comment>
+        <translation>O</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget.py" line="447"/>
+        <source>E</source>
+        <comment>lblEast</comment>
+        <translation>E</translation>
+    </message>
+</context>
+<context>
+    <name>CoordinatorDockWidgetBase</name>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="59"/>
+        <source>Select Input CRS</source>
+        <translation>Sélectionner le CRS en entré</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="62"/>
+        <source>input CRS</source>
+        <translation>CRS en entrée</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="100"/>
+        <source>Show Help</source>
+        <translation>Afficher l&apos;aide</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="160"/>
+        <source>Show Coordinates on Map</source>
+        <translation>Afficher les coordonnées sur la carte</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="207"/>
+        <source>Pan Map to Location</source>
+        <translation>Zoom sur l&apos;emprise totale</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="273"/>
+        <source>Pick Point from Map (Shift+click grabs also the project&apos;s CRS)</source>
+        <translation>Récupérer point depuis la carte (Shift + click pour saisir aussi le CRS du projet)</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="672"/>
+        <source>toggle Hemisphere</source>
+        <comment>btCardinalLeft</comment>
+        <translation>bascule O/E</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="675"/>
+        <source>E</source>
+        <comment>lblEast</comment>
+        <translation>E</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1056"/>
+        <source>Toggle Hemisphere</source>
+        <comment>btCardinalLeft</comment>
+        <translation>bascule N/S</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1059"/>
+        <source>N</source>
+        <comment>lblNorth</comment>
+        <translation>N</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1100"/>
+        <source>Clear Input</source>
+        <translation>Effacer la donnée en entrée</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1138"/>
+        <source>Geographic Coordinates as</source>
+        <translation>Coordonnées géographiques</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1346"/>
+        <source>Decimal</source>
+        <translation>Décimale</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1362"/>
+        <source>DMS</source>
+        <translation>DMS</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1261"/>
+        <source>Coordinate Transformation</source>
+        <translation>Coordonnées transformées</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1281"/>
+        <source>Select Output CRS</source>
+        <translation>Sélectionner le CRS en sortie</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1284"/>
+        <source>output CRS</source>
+        <translation>CRS de sortie</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1309"/>
+        <source>Set Output CRS to follow Layer Selection</source>
+        <translation>Configurer le CRS de sortie en fonction de la couche sélectionnée</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1389"/>
+        <source>Longitude/Easting</source>
+        <translation>Longitude</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1502"/>
+        <source>Copy Result to Clipboard</source>
+        <translation>Copier le résultat dans le presse papier</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1477"/>
+        <source>Latitude/Northing</source>
+        <translation>Latitude</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1580"/>
+        <source>Copy whole Result to Clipboard</source>
+        <translation>Copier l&apos;ensemble du résultat dans le presse papier</translation>
+    </message>
+    <message>
+        <location filename="../coordinator_dockwidget_base.ui" line="1621"/>
+        <source>Add Coordinates to currently edited Feature</source>
+        <translation>Utiliser les coordonnées pour créer un point dans la couche en cours d&apos;édition</translation>
+    </message>
+</context>
+</TS>
