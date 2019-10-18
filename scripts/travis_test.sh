84d0c0fcb49425d0e4a0f2e3830433f40ec7153d
iona5 <github@ag99.de>
2019-10-18 13:51:29 +0200

    changes in travis test script to actually let build fail if tests failed
    the current script does not fail if tests fail because the "set" command
    at the beginning was wrong.
    
    also we now want to actually handle the test script results internally, so
    an instant fail is now disabled, we print a nice message and then return
    with the exit code of the testrunner script.

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set +ex
+set -x
 if [ -z $1 ] ; then QGIS_RELEASE=$1 ; fi
 
 DOCKER_TAG=release-${QGIS_RELEASE}
@@ -12,8 +12,15 @@ docker run -d --name qgis-${QGIS_RELEASE} -e DISPLAY=:99 qgis/qgis:${DOCKER_TAG}
 ./docker_test_setup.sh qgis-${QGIS_RELEASE}
 sleep 2
 docker exec -t qgis-${QGIS_RELEASE} sh -c "qgis_testrunner.sh coordinator.test.run_all"
+RESULT=$?
 docker stop qgis-${QGIS_RELEASE}
 docker rm qgis-${QGIS_RELEASE}
 
+if [ ${RESULT} = 0 ]; then
+	echo " ----------- ALL TESTS PASSED ---------------- "
+else
+	echo " !!!!!!!!!! SOME TESTS FAILED !!!!!!!!!!!!!!!! "
+fi
 
+exit ${RESULT}
 
