9a20376a8b795a26cf85c2ad9aae683db43b651e
iona5 <github@ag99.de>
2020-12-27 12:04:48 +0100

    testenv: replace travis builds QGIS 3.12 -> 3.16

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -9,9 +9,6 @@ fi
 
 DOCKER_TAG=release-${QGIS_RELEASE}
 
-# -- 3.8 does not have "release-*" dockerfiles so we need to track that manually :(
-#if [ ${QGIS_RELEASE} = 3_8 ]; then DOCKER_TAG=final-3_8_3 ; fi
-
 docker run -d --rm --name qgis-${QGIS_RELEASE} -e DISPLAY=:99 qgis/qgis:${DOCKER_TAG}
 ./docker_test_setup.sh qgis-${QGIS_RELEASE}
 sleep 2
