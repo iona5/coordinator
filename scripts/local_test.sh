ac5ecf2c9c3d9c419d3129119a91990d666ae621
iona5 <github@ag99.de>
2020-12-29 13:26:04 +0100

    testenv: add a script to run tests locally via docker

---
+++ scripts/local_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+if [ -z $1 ] ; then 
+	QGIS_RELEASE=3_10
+else 
+	QGIS_RELEASE=$1
+fi
+
+DOCKER_TAG=release-${QGIS_RELEASE}
+CONTAINER=qgis-${QGIS_RELEASE}
+
+
+docker run -d --rm --name qgis-${QGIS_RELEASE} -e DISPLAY=:99 qgis/qgis:${DOCKER_TAG}
+# display version:
+docker exec ${CONTAINER} sh -c "qgis --version 2>&1 | head -n 1"
+
+docker exec ${CONTAINER} sh -c "mkdir -p /tests_directory"
+docker cp ../coordinator ${CONTAINER}:/tests_directory/coordinator
+docker exec ${CONTAINER} sh -c "qgis_setup.sh coordinator"
+
+if [ ${QGIS_RELEASE} = "3_16" ] ; then
+	docker exec -t ${CONTAINER} sh -c "apt install python3-pyqt5.qtwebkit"
+fi
+
+sleep 2
+
+#docker exec -it ${CONTAINER} /bin/bash
+
+set +e
+docker exec -t ${CONTAINER} sh -c "cd /tests_directory && python3 -m coordinator.test"
+RESULT=$?
+set -e
+
+docker stop ${CONTAINER}
+
+if [ ${RESULT} = 0 ]; then
+	echo " ----------- ALL TESTS PASSED ---------------- "
+else
+	echo " !!!!!!!!!! SOME TESTS FAILED !!!!!!!!!!!!!!!! "
+fi
+
+exit ${RESULT}
