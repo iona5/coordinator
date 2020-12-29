23df0b088e916d415fe44fd7ff1e7cd9633272ae
iona5 <github@ag99.de>
2020-12-29 11:42:40 +0100

    testenv: remove docker container after finishing

--- scripts/travis_test.sh
+++ scripts/travis_test.sh
@@ -12,13 +12,12 @@ DOCKER_TAG=release-${QGIS_RELEASE}
 # -- 3.8 does not have "release-*" dockerfiles so we need to track that manually :(
 #if [ ${QGIS_RELEASE} = 3_8 ]; then DOCKER_TAG=final-3_8_3 ; fi
 
-docker run -d --name qgis-${QGIS_RELEASE} -e DISPLAY=:99 qgis/qgis:${DOCKER_TAG}
+docker run -d --rm --name qgis-${QGIS_RELEASE} -e DISPLAY=:99 qgis/qgis:${DOCKER_TAG}
 ./docker_test_setup.sh qgis-${QGIS_RELEASE}
 sleep 2
 docker exec -t qgis-${QGIS_RELEASE} sh -c "qgis_testrunner.sh coordinator.test.run_all"
 RESULT=$?
 docker stop qgis-${QGIS_RELEASE}
-docker rm qgis-${QGIS_RELEASE}
 
 if [ ${RESULT} = 0 ]; then
 	echo " ----------- ALL TESTS PASSED ---------------- "
